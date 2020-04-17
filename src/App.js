3a56806dc6ee59257342f0df1678f52d52e7e5c3
Andrew London <london.andrewd@gmail.com>
2020-04-16 22:48:21 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { reducer as voxeetReducer } from '@voxeet/react-components';
+import React from 'react'
+import thunkMidleware from 'redux-thunk'
+import { combineReducers, createStore, applyMiddleware } from 'redux'
+
+import { ConferenceRoom, VoxeetProvider } from '@voxeet/react-components'
+
+// Import Style
+import '@voxeet/react-components/dist/voxeet-react-components.css';
+
+const reducers = combineReducers({
+  voxeet: voxeetReducer
+});
+
+const configureStore = () => createStore(
+  reducers,
+  applyMiddleware(thunkMidleware)
+)
+
+const settings = {
+  consumerKey: '',
+  consumerSecret: '',
+  conferenceAlias: 'Sample'
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <VoxeetProvider store={configureStore()}>
+      <ConferenceRoom
+        autoJoin
+        consumerKey={settings.consumerKey}
+        consumerSecret={settings.consumerSecret}
+        conferenceAlias={settings.conferenceAlias}
+      />
+    </VoxeetProvider>
   );
 }
 
